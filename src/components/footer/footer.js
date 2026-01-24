7cb17a96a92ef84ada4948e5675d5545659d5c92
Tiago Lucas <147514025+tiagolucasoo@users.noreply.github.com>
2026-01-24 09:36:45 -0300

    Update footer copyright text for 2023

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
     <footer>
         <div className="container">
-                <p>© {new Date().getFullYear()} - Diário de Bordo | Extensão IFPR Londrina</p>
+                <p>© {new Date().getFullYear()} - Extensão IFPR Londrina 2023</p>
                 <p style={{ fontSize: '0.9rem', marginTop: '8px' }}>
                     Desenvolvido por <a href="https://github.com/tiagolucasoo" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--color-primary)', fontWeight: 'bold', marginLeft: '5px' }}>
                         Tiago Lucas Oliveira
